a49892dd481ec117b0af507b65edae3bea5d40fb
KeenGWatanabe <mckeenweb@gmail.com>
2025-04-16 19:18:40 +0800

    fix vpc

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   # vpc_id = module.vpc.vpc_id  # Example of inter-module dependency
 }
 
-module "ecs" {
-  source = "./modules/ecs"
+# module "ecs" {
+#   source = "./modules/ecs"
   
-}
+# }
--- infra/modules/vpc/ecs-vpc.tf
+++ infra/modules/vpc/ecs-vpc.tf
@@ -17,3 +17,49 @@ resource "aws_subnet" "public" {
     Name = "rger-flask-public-${count.index}"
   }
 }
+
+# igw
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "rger-flask-igw"
+  }
+}
+
+# rtb-pub
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "rger-flask-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# vpc endpoint Secrets Mgr (optional but recommended)
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.us-east-1.secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.public[*].id  # Attach to public subnets
+  security_group_ids  = [aws_security_group.vpc_endpoint.id]
+  private_dns_enabled = true
+}
+
+resource "aws_security_group" "vpc_endpoint" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]  # Restrict to VPC
+  }
+}
